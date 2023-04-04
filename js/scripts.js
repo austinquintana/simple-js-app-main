445c6810d90e8224d15dde4e66ab441917fbc032
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-04-03 19:59:22 -0600

    Added some extra comments for organization of each Pokémon page

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 // empty array 
 let pokemonList = [];
 
+// first page 
 pokemonList[0] = {
     name: "Squirtle",
     height: 0.5, 
     types: ["Water"]
 };
 
+// second page
 pokemonList[1] = {
     name: "Charmander",
     height: 0.6,
     types: ["Fire"]
 };
 
+//third page
 pokemonList[2] = {
     name: "Bulbasaur",
     height: 0.7,
     types: ["Grass", "Poison"]
 };
 
+// testing the pages
 console.log(pokemonList);
