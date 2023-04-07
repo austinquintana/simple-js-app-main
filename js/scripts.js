66f733acc8a20480cd5f168acd6d18848a90ca0d
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-04-06 20:22:09 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,21 @@ let pokeDex = (function() {
 pokemonList[0] = {
     name: "Squirtle",
     height: 0.5, 
-    types: ["Water"]
+    types: ["water"]
 };
 
 // second page
 pokemonList[1] = {
     name: "Charmander",
     height: 0.6,
-    types: ["Fire"]
+    types: ["fire"]
 };
 
 //third page
 pokemonList[2] = {
     name: "Bulbasaur",
     height: 0.7,
-    types: ["Grass", "Poison"]
+    types: ["grass", "poison"]
 };
 
 function add(pokemon) {
@@ -39,16 +39,17 @@ return {
 })();
 
 pokeDex.getAll().forEach (function(pokemon){
-    document.write (pokemon.name+ ', ' + pokemon.height + ', is a' + pokemon.type + 'type pokemon.<br>')
+    document.write (pokemon.name+ ', ' + pokemon.height + ', is a ' + pokemon.types + ' type pokemon.<br>')
+
 });
 
-for (let i = 0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + " " + pokemonList[i].height + ")" + " ");
+for (let i = 0; i < pokemonList.height; i++){
+if (pokemonList[i].height > 1.3){
 
-    if (pokemonList[i].height > 1.3){
-        document.write("-Wow that's a big Pokemon");
-    } else {
-        document.write(" -Little Man!")
-    }
-    document.write("<br>")
+    document.write("-Wow that's a big Pokemon");
+} else {
+    document.write(" -Little Man!")
+}
+document.write("<br>")
+
 };
