c94e4b2a604a637ca29d50e8b1d3d6a029ab8028
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-04-07 20:20:15 -0600

    Continued to add more functions to the pokedex app while also adding style design.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,61 @@
 // empty array 
-let pokeDex = (function() {
+let pokeDex = (function () {
     let pokemonList = [];
 
-// first page 
-pokemonList[0] = {
-    name: "Squirtle",
-    height: 0.5, 
-    types: ["water"]
-};
+        // first page 
+        pokemonList[0] = {
+            name: "Squirtle",
+            height: 0.5, 
+            types: ["water"]
+        };
 
-// second page
-pokemonList[1] = {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"]
-};
+        // second page
+        pokemonList[1] = {
+            name: "Charmander",
+            height: 0.6,
+                types: ["fire"]
+        };
 
-//third page
-pokemonList[2] = {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"]
-};
+        //third page
+        pokemonList[2] = {
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["grass", "poison"]
+        };
 
-function add(pokemon) {
-    pokeDex.push(pokemon);
-}
+    function add(pokemon) {
+        pokeDex.push(pokemon);
+    }
 
-function getAll(){
-    return pokemonList;
-}
+    function getAll(){
+        return pokemonList;
+    }
 
-return {
-    add: add,
-    getAll: getAll
-};
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+         let button = document.createElement("button");
+        button.innerText = pokemon.name; 
+        button.classList.add("button-class"); 
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        button.addEventListener("click", function (event) {
+            showDetails(pokemon)
+        });
+     }
 
+     function showDetails(pokemon) {
+        console.log(pokemon.name)
+     }
+
+    return {
+        add: add,
+        getAll: getAll,
+         addListItem: addListItem
+    };
 })();
 
-pokeDex.getAll().forEach (function(pokemon){
-    document.write (pokemon.name+ ', ' + pokemon.height + ', is a ' + pokemon.types + ' type pokemon.<br>')
-
+pokeDex.getAll().forEach(function(pokemon) {
+    
+    pokeDex.addListItem(pokemon);
 });
-
-for (let i = 0; i < pokemonList.height; i++){
-if (pokemonList[i].height > 1.3){
-
-    document.write("-Wow that's a big Pokemon");
-} else {
-    document.write(" -Little Man!")
-}
-document.write("<br>")
-
-};
