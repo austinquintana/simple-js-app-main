ddee8fd9ea23472cacf9cd817265b712da8c1fe9
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-04-08 18:03:05 -0600

    Added modals to display whenever you clicked on a certain pokemon. Added more web design and comments.

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,30 @@ let pokeDex = (function () {
         return pokemonList;
     }
 
+    // creates a list of buttons for the pokemon names and includes an event listener. Which allows the user to "click" to find out more details on that specfic pokemon
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemon-list");
+        pokemonList.classList.add("list-group");
+
         let listpokemon = document.createElement("li");
-         let button = document.createElement("button");
+        listpokemon.classList.add("list-group-item");
+
+        let button = document.createElement("button");
         button.innerText = pokemon.name; 
-        button.classList.add("button-class"); 
+        button.classList.add("pokemon"); 
+        button.classList.add("btn");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", ".modal");
+
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
+
         button.addEventListener("click", function (event) {
             showDetails(pokemon);
         });
      }
     
+     // fetches pokemon info from the API and catches any errors 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
           return response.json();
@@ -34,19 +45,20 @@ let pokeDex = (function () {
               detailsUrl: item.url
             };
             add(pokemon);
-            console.log(pokemon);
           });
         }).catch(function (e) {
           console.error(e);
         })
       }
 
+    //Specifies specific details about the pokemon pulled from the API
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
           return response.json();
         }).then(function (details) {
           // Now we add the details to the item
+          item.id = details.id
           item.imageUrl = details.sprites.front_default;
           item.height = details.height;
           item.types = details.types;
@@ -54,12 +66,41 @@ let pokeDex = (function () {
           console.error(e);
         });
       }
-
-     function showDetails(item) {
-        pokeDex.loadDetails(item).then(function() {
-        console.log(item)
+ 
+      //Displays the details of said pokemon in a modal 
+     function showDetails(pokemon) {
+        pokeDex.loadDetails(pokemon).then(function() {
+        showModal(pokemon);
      });
     }
+
+    // displays a modal
+    function showModal(item) {
+
+        pokeDex.loadDetails(item).then(function () {
+
+        let modalNumber = document.querySelector(".modal-number")
+        modalNumber.innerHTML = "#" + item.id;
+
+        let modalTitle = document.querySelector(".modal-title");
+        modalTitle.innerText = ". " + item.name;
+
+        let pokemonHeight = document.querySelector(".pokemon-height");
+        pokemonHeight.innerHTML = "Height: " + "<br>" + (item.height/10) + "m";
+
+        let itemTypes = "";
+        item.types.forEach(function(types) {
+          itemTypes += [types.type.name + "<br>"];
+        });
+
+        let pokemonTypes = document.querySelector(".pokemon-types");
+        pokemonTypes.innerHTML = "Types: " + "<br>" + itemTypes;
+
+        let pokemonImg = document.querySelector(".pokemon-img");
+        pokemonImg.src = item.imageUrl;
+    
+    });
+  }
 
     return {
         add: add,
@@ -67,7 +108,8 @@ let pokeDex = (function () {
         addListItem: addListItem,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
+        showDetails: showDetails,
+        showModal: showModal
     };
 })();
 
